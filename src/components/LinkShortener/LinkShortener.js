c9e38073f42d07abcb280b446ca6bc0d8276a82b
“ericelvendahl” <ericelvendahl@gmail.com>
2020-09-01 10:17:03 -0500

    copy link button working

--- src/components/LinkShortener/LinkShortener.js
+++ src/components/LinkShortener/LinkShortener.js
@@ -12,6 +12,7 @@ class LinkShortener extends Component {
   state = {
     inputUrl: "",
     shortenedUrl: "",
+    copySuccess: "",
   };
 
   buttonClicked = () => {
@@ -19,13 +20,34 @@ class LinkShortener extends Component {
     this.setState({
       shortenedUrl: "https://wespark.le/" + shortId.generate(),
     });
-  }; // End buttonClicked()
+    console.log(
+      "In buttonClicked. this.state.shortenedUrl is",
+      this.state.shortenedUrl
+    );
+    this.props.dispatch({
+      type: "ADD_LINK",
+      payload: {
+        inputURL: this.state.inputUrl,
+        shortenedUrl: this.state.shortenedUrl,
+      },
+    });
+  }; // end buttonClicked()
+
+  copyToClipboard = (e) => {
+    this.textArea.select();
+    document.execCommand("copy");
+    // Next two lines from example code:
+    // This is just personal preference.
+    // I prefer to not show the whole text area selected.
+    e.target.focus();
+    this.setState({ copySuccess: "Link copied!" });
+  }; // end copyToClipboard()
 
   handleInputChangeFor = (propertyName) => (event) => {
     this.setState({
       [propertyName]: event.target.value,
     });
-  }; // End handleInputChangeFor()
+  }; // end handleInputChangeFor()
 
   render() {
     return (
@@ -38,13 +60,32 @@ class LinkShortener extends Component {
             onChange={this.handleInputChangeFor("inputUrl")}
           />
           <button onClick={this.buttonClicked}>Generate</button>
-          <br />Your shortened link:
-          <input
-            type="text"
+          <br />
+          Your shortened link:
+          <br />
+          {/* <input type="text" name="username" value={this.state.shortenedUrl} /> */}
+          <textarea
+            ref={(textarea) => (this.textArea = textarea)}
             name="username"
             value={this.state.shortenedUrl}
           />
-          
+          {/* From example at: https://stackoverflow.com/questions/39501289/in-reactjs-how-to-copy-text-to-clipboard */}
+          <div>
+            {
+              /* Logical shortcut for only displaying the 
+            button if the copy command exists */
+              document.queryCommandSupported("copy") && (
+                <div>
+                  <button onClick={this.copyToClipboard}>
+                    Copy Shortened Link
+                  </button>
+                  {this.state.copySuccess}
+                </div>
+              )
+            }
+            
+            this.state.shortenedUrl is: {JSON.stringify(this.state.shortenedUrl)}
+          </div>
         </div>
       </center>
     ); // end return
