4d7a806b56ccf1b22660761375078d344db0c52a
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-09-02 16:01:36 -0500

    changed routes in app.js and component locations in landing/advancedLanding

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,8 +16,12 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
+import Landing from '../Landing/Landing';
+import AdvancedLanding from '../AdvancedLanding/AdvancedLanding';
 
 import './App.css';
+import LoginPage from '../LoginPage/LoginPage';
+import RegisterPage from '../RegisterPage/RegisterPage';
 
 class App extends Component {
   componentDidMount () {
@@ -36,17 +40,28 @@ class App extends Component {
             This is a route anyone can see, no login necessary */}
             <Route
               exact
-              path="/about"
-              component={AboutPage}
+              path="/home"
+              component={Landing}
             />
+            <Route 
+              exact
+              path='/login'
+              component={LoginPage}
+            />
+            <Route 
+              exact
+              path='/register'
+              component={RegisterPage}
+            />
+
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
             <ProtectedRoute
               exact
-              path="/home"
-              component={UserPage}
+              path="/protected"
+              component={AdvancedLanding}
             />
             {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
