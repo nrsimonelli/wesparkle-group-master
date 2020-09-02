e5509d211369cb76f59e3d28a842515dde4dfece
Audry Wolters <audrywolters@gmail.com>
2020-09-02 10:01:21 -0500

    env file is working

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -1,40 +1,24 @@
 const express = require('express');
-// no DB needed so far -- const pool = require('../modules/pool');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 
+// BUGS in HEROKU? https://www.twilio.com/blog/working-with-environment-variables-in-node-js-html
 
 router.post('/', (req, res) => {
 
-    // where is my data? i want this from the saga call!:
-      // payload: {
-      //   emailBody: this.state.emailBody,
-      //   userEmail: this.state.userEmail,
-      //   userName: this.state.userName
-      // }
-
-    console.log( 'req body', req.body );
-    // req.body = {} empty!
-
-
     // this is the ship the user's email rides
     var transport = nodemailer.createTransport({
-      // AUDRY - we need to contact WeSparkle for this info. 
-      // They've got it set up already, we just need their 
-      // host, port, email, password
       host: "smtp.mailtrap.io",
       port: 2525,
       auth: {
-        user: "96acbdbff9f2ca",  // user: process.env.EMAIL || 'abc@gmail.com', 
-        pass: "055545b4aa2cfc"   // pass: process.env.PASSWORD || '1234'
+        user: process.env.FEEDBACK_EMAIL_USERNAME || 'abc@gmail.com', // AUDRY - do we want an OR?
+        pass: process.env.FEEDBACK_EMAIL_PASSWORD || '1234' // AUDRY - do we want an OR?
       }
     });
 
     // here's where we set who does what
     let mailOptions = {
-      // AUDRY - from...
-      //from: paxossparkles-2038ac@inbox.mailtrap.io
-      to: 'paxossparkles-2038ac@inbox.mailtrap.io',   // process.env.EMAIL || 'abc@gmail.com'
+      to: process.env.FEEDBACK_EMAIL || 'abc@gmail.com',
       text: `from: ${req.body.userName}, their email: ${req.body.userEmail}, and their feedback: ${req.body.emailBody}`
     };
 
@@ -47,13 +31,4 @@ router.post('/', (req, res) => {
     });
 })
   
-
-
-
-
-
-// FEEDBACK_EMAIL
-// FEEDBACK_EMAIL_PASSWORD
-// sendmail, a regular sendmail command for simple messages.
-
 module.exports = router;
