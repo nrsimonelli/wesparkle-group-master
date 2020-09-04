7e8d5b3a396844af0a2f936bf06c6fd8c4080313
“ericelvendahl” <ericelvendahl@gmail.com>
2020-09-04 10:38:12 -0500

    begin url-parse work

--- src/components/LinkShortener/LinkShortener.js
+++ src/components/LinkShortener/LinkShortener.js
@@ -2,10 +2,10 @@ import shortId from "shortid";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import QRCode from "qrcode.react";
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 import copy from "clipboard-copy";
-
+import parse from "url-parse";
 
 // corresponds to 1.0
 class LinkShortener extends Component {
@@ -18,6 +18,20 @@ class LinkShortener extends Component {
     shortenedUrl: "",
     copySuccess: "",
   };
+
+  copyClicked = (e) => {
+    this.textArea.select();
+    document.execCommand("copy");
+    // Next two lines from example code:
+    // This is just personal preference.
+    // I prefer to not show the whole text area selected.
+    e.target.focus();
+    this.setState({
+      copySuccess: "Link copied!",
+      inputUrl: "",
+      shortenedUrl: "",
+    });
+  }; // end copyClicked()
 
   generateClicked = () => {
     console.log("Button clicked");
@@ -34,6 +48,9 @@ class LinkShortener extends Component {
       "In generateClicked. this.state.shortenedUrl is",
       this.state.shortenedUrl
     );
+    // Use url-parse library (as 'parse') to clean input URL
+    let cleanUrl = this.state.inputUrl;
+    cleanUrl = parse(cleanUrl);
     this.props.dispatch({
       type: "ADD_LINK",
       payload: {
@@ -44,20 +61,6 @@ class LinkShortener extends Component {
     });
   }; // end generateClicked()
 
-  copyClicked = (e) => {
-    this.textArea.select();
-    document.execCommand("copy");
-    // Next two lines from example code:
-    // This is just personal preference.
-    // I prefer to not show the whole text area selected.
-    e.target.focus();
-    this.setState({
-      copySuccess: "Link copied!",
-      inputUrl: "",
-      shortenedUrl: "",
-    });
-  }; // end copyClicked()
-
   handleInputChangeFor = (propertyName) => (event) => {
     this.setState({
       [propertyName]: event.target.value,
@@ -66,81 +69,71 @@ class LinkShortener extends Component {
 
   render() {
     return (
-      <div className='container link-shortener'>
-       
-          <TextField
-            id="outlined-link-input"
-            label="Type link here"
-            type="text"
-            className="text-field"
-            name="link"
-            margin="normal"
-            variant="outlined"
-            value={this.state.inputUrl}
-            onChange={this.handleInputChangeFor("inputUrl")}
-          />
-          <Button
-            id='generate' 
-            className='big'
-            onClick={this.generateClicked}
-            variant='outlined'
-            color='default'
-            >Generate
-          </Button>
-          <div>
-          Your shortened link:
-          </div>
-    
-          <textarea
-            
-            type='text'
-            className='text-area'
-            ref={(textArea) => (this.textArea = textArea)}
-            name="shorturl"
-            defaultValue={this.state.shortenedUrl}
-          />
-          {/* From example at: https://stackoverflow.com/questions/39501289/in-reactjs-how-to-copy-text-to-clipboard */}
-          <div className='qr-div'>
-            {
-              /* Logical shortcut for only displaying the 
+      <div className="container link-shortener">
+        <TextField
+          id="outlined-link-input"
+          label="Type link here"
+          type="text"
+          className="text-field"
+          name="link"
+          margin="normal"
+          variant="outlined"
+          value={this.state.inputUrl}
+          onChange={this.handleInputChangeFor("inputUrl")}
+        />
+        <Button
+          id="generate"
+          className="big"
+          onClick={this.generateClicked}
+          variant="outlined"
+          color="default"
+        >
+          Generate
+        </Button>
+        <div>Your shortened link:</div>
+
+        <textarea
+          type="text"
+          className="text-area"
+          ref={(textArea) => (this.textArea = textArea)}
+          name="shorturl"
+          defaultValue={this.state.shortenedUrl}
+        />
+        {/* From example at: https://stackoverflow.com/questions/39501289/in-reactjs-how-to-copy-text-to-clipboard */}
+        <div className="qr-div">
+          {
+            /* Logical shortcut for only displaying the 
             button if the copy command exists */
-              document.queryCommandSupported("copy") && (
-                <div>
-                  <Button 
-                    id='copy'
-                    className='big'
-                    onClick={this.copyClicked}
-                    variant='outlined'
-                    color='primary'
-                    >
-                    Copy Shortened Link
-                  </Button>
-                  <center>
-                  {this.state.copySuccess}
-                  </center>
-                </div>
-                
-              )
-            }
+            document.queryCommandSupported("copy") && (
+              <div>
+                <Button
+                  id="copy"
+                  className="big"
+                  onClick={this.copyClicked}
+                  variant="outlined"
+                  color="primary"
+                >
+                  Copy Shortened Link
+                </Button>
+                <center>{this.state.copySuccess}</center>
+              </div>
+            )
+          }
 
-            {/* Render QR code only if URL has been submitted */}
-            <div>
-              {this.state.shortenedUrl !== "" ? (
-                <center>
-                  <QRCode className='qr' value={this.state.shortenedUrl} />
-                  <br />
-                  Right click or long press to save image
-                </center>
-              ) : (
-                <div />
-              )}
-            </div>
-
-          </div>            
-              {/* {JSON.stringify(this.state.shortenedUrl)} */}
-           
-          
-        
+          {/* Render QR code only if URL has been submitted */}
+          <div>
+            {this.state.shortenedUrl !== "" ? (
+              <center>
+                <QRCode className="qr" value={this.state.shortenedUrl} />
+                <br />
+                Right click or long press to save image
+              </center>
+            ) : (
+              <div />
+            )}
+          </div>
+        </div>
+        {/* {JSON.stringify(this.state.shortenedUrl)} */}
       </div>
     ); // end return
   } // end render
