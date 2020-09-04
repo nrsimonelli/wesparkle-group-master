137cec83d6519b5bf99c95c74a9ac9ff472e3418
Audry Wolters <audrywolters@gmail.com>
2020-09-04 15:32:47 -0500

    removed InfoPage from Nav. Cleaned up Feedback.Router

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -21,9 +21,6 @@ const Nav = (props) => (
       {/* Show the link to the info page and the logout button if the user is logged in */}
       {props.user.id && (
         <>
-          <Link className="nav-link" to="/info">
-            Rename Me
-          </Link>
           <LogOutButton className="nav-link"/>
         </>
       )}
