d8a0a5f623cb06dee2fcd371ec8806d0c1ee2407
Audry Wolters <audrywolters@gmail.com>
2020-09-09 18:38:01 -0500

    not using pics. using blue background.

--- src/components/FeedbackCarousel/FeedbackCarousel.js
+++ src/components/FeedbackCarousel/FeedbackCarousel.js
@@ -5,36 +5,30 @@ import { Carousel } from "react-responsive-carousel";
 import "./FeedbackCarousel.css";
 
 class FeedbackCarousel extends Component {
-  componentDidMount() {
-    console.log("did mount FeedbackCarousel");
-  }
 
   render() {
     return (
       <div className='carousel-flex'>
-          <Carousel showThumbs={false} showStatus={false} infiniteLoop className="feedbackCarousel">
+          <Carousel autoplay infiniteLoop showThumbs={false} showStatus={false} className="feedbackCarousel">
             <div>
-              <div className="feedbackCarousel image-box image-circle"></div>
               <p>
-                "This link shortener is so easy to use. Thank you!"
+                " This link shortener is so easy to use. Thank you! "
                 <br /><br />
                  - Margaret T.
               </p>
             </div>
 
             <div>
-              <div className="feedbackCarousel image-box image-jump"></div>
               <p>
-                "It's nice to be able to shorten my links for free and give back to the community at the same time!"
+                " It's nice to be able to shorten my links for free and give back to the community at the same time! "
                 <br /><br />
                 - Nick M.
               </p>
 
             </div>
             <div>
-              <div className="feedbackCarousel image-box image-bigPhone"></div>           
               <p>
-                "Loved printing my QR code on my business cards. Thanks!"
+                " Loved printing my QR code on my business cards. Thanks! "
                 <br /><br />
                 - Rebecca F.
               </p>
