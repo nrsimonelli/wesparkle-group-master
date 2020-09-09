ef951243a439c8c3af96a829e4c5024efffcaa63
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-09-09 16:25:57 -0500

    created graph router, reducer, saga, and carved out sql query

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const linkRouter = require('./routes/link.router');
 const feedbackRouter = require('./routes/feedback.router');
 const detailsRouter = require('./routes/details.router');
 const tagsRouter = require('./routes/tags.router');
+const graphRouter = require('./routes/graph.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -32,6 +33,7 @@ app.use('/api/link', linkRouter);
 app.use('/api/feedback', feedbackRouter);
 app.use('/api/details', detailsRouter);
 app.use('/api/tags', tagsRouter);
+app.use('/api/graph', graphRouter);
 
 // Serve static files
 app.use(express.static('build'));
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,6 +4,7 @@ import loginMode from './loginModeReducer';
 import user from './userReducer';
 import link from './linkReducer';
 import details from './linkDetailsReducer';
+import graph from './graphReducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -17,6 +18,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   link,
   details,
+  graph,
 });
 
 export default rootReducer;
