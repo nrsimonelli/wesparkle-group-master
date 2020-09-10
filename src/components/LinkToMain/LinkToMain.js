b9d28b3bcac8a2a0a8373bb0b71aeae1a4b1f927
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-09-10 09:35:31 -0500

    copy organization

--- src/components/LinkToMain/LinkToMain.js
+++ src/components/LinkToMain/LinkToMain.js
@@ -13,10 +13,10 @@ class LinkToMain extends Component {
     return (
       <div className='link-to-main container'>
 
-        <div className='support description'>A different but very similar discription than the one above about the same topic. should be the same style and lead to the same place.</div>
+        <div className='support description'></div>
         <div className='image-title link-button'>
         <a href='https://www.wesparkle.org/' target='blank'>
-        Learn More About how we help small businesses
+        Learn more about how we help small businesses!
         </a>
         </div>
       </div>
