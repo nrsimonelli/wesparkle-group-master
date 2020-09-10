b7abd9149a584169241dee24140bf5d91475e732
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-09-09 21:28:47 -0500

    added client copy to benefits and link component

--- src/components/BenefitsCopy/BenefitsCopy.js
+++ src/components/BenefitsCopy/BenefitsCopy.js
@@ -13,16 +13,34 @@ class BenefitsCopy extends Component {
     return (
       <div className='benefits container'>
         <div className='benefits image-title'>
-          Benefits of Link Shortening Part One
+          Why make a short link?
         </div>
         <div className='benefits image-box image-1'>
 
         </div>
+        <div className='support description'>
+        - Makes it easy for people to remember your website or link!
+        </div>
+        <div className='support description'>
+        - Some social media channels like Twitter have character limits.
+        </div>
+        <div className='support description'>
+        - Makes your social media posts, flyers, and emails look more professional.
+        </div>
         <div className='benefits image-title'>
-          Benefits of Link Shortening Part Two
+          Why make a QR code?
         </div>
         <div className='benefits image-box image-2'>
 
+        </div>
+        <div className='support description'>
+        - Makes it easy for people to use their phones to scan and be directed to the correct website.   
+        </div>      
+        <div className='support description'>
+        - People don't have to remember your website or type it into their phone/laptop (and accidentally misspell it).        
+        </div>
+        <div className='support description'>
+        - QR codes are graphics that can sometimes be more eye-catching than a website link.
         </div>
         <div className='image-title link-button'>
         <a href='https://www.wesparkle.org/' target='blank'>
