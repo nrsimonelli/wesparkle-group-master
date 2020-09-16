0e422886601c232d8b612340655ec582352a65f5
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-09-16 10:50:34 -0500

    commented and cleaned graph.router

--- server/routes/graph.router.js
+++ server/routes/graph.router.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
+
+//  Part of logged user features only
 const {
   rejectUnauthenticated,
 } = require("../modules/authentication-middleware");
 
 router.get("/:id", rejectUnauthenticated, (req, res) => {
-  //Will eventually have to edit this to include tags
-  console.log("details get for req.params.id:", req.params.id);
-  console.log("details get for req.user.id:", req.user.id);
+  // This query will return clicks per link by date for the logged in user
   const query = `
     SELECT DATE(timestamp) AS x, COUNT (*) AS y
     FROM click 
     WHERE link_id = $1
     GROUP BY DATE(timestamp)
     ORDER BY x;`;
+
   pool
     .query(query, [req.params.id])
     .then((result) => {
@@ -25,7 +26,5 @@ router.get("/:id", rejectUnauthenticated, (req, res) => {
       res.sendStatus(500);
     });
 });
-
-
 
 module.exports = router;
