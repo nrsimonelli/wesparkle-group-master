26b44a10b42868d1edfa2678423eec211a489da5
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-09-16 10:36:46 -0500

    graphTwo checked and commented

--- src/components/GraphTwo/GraphTwo.js
+++ src/components/GraphTwo/GraphTwo.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import {XYPlot, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, VerticalBarSeries, VerticalBarSeriesCanvas} from 'react-vis';
 
 
-
+// sample data
 const data = [
   {x: 'Roster Management', y: 1925},
   {x: 'Why We Sleep', y: 922},
@@ -11,15 +11,12 @@ const data = [
 ]
 
 class GraphTwo extends Component {
-  
-  componentDidMount () {
-    console.log('componentDidMount g2');
-  }
 
   render() {
     return (
       <>
         <XYPlot
+        // type ordinal for strings
           xType={'ordinal'}
           width={380}
           height={500}>
@@ -30,7 +27,6 @@ class GraphTwo extends Component {
           <VerticalBarSeriesCanvas
             data={data}
             color={'#0c1466ff'} />
-          
         </XYPlot>
       </>
     ) // end return
