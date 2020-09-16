c8f4d32f3d970e880c1abea692f8a61fa0a4a63d
Joel Roske <joel.j.roske@gmail.com>
2020-09-16 10:54:01 -0500

    details router cleaned

--- server/routes/details.router.js
+++ server/routes/details.router.js
@@ -6,9 +6,6 @@ const {
 } = require("../modules/authentication-middleware");
 
 router.get("/:id", rejectUnauthenticated, (req, res) => {
-  //Will eventually have to edit this to include tags
-  console.log("details get for req.params.id:", req.params.id);
-  console.log("details get for req.user.id:", req.user.id);
   const query = `
   SELECT * from link
 WHERE "user_id" = $2 AND "id" = $1
@@ -19,17 +16,12 @@ WHERE "user_id" = $2 AND "id" = $1
       res.send(result.rows[0]);
     })
     .catch((error) => {
-      console.log("Error making SELECT for details:", error);
+      console.log(error);
       res.sendStatus(500);
     });
 });
 
-//PUT template, doesn't do anything yet unless we need it
 router.put("/:id", rejectUnauthenticated, (req, res) => {
-  console.log("PUT req.body.tags: ", req.body.tags);
-  console.log("PUT req.body.details.id: ", req.body.details.id);
-  console.log("PUT req.user.id: ", req.user.id);
-
   const link = req.body;
   const queryString = `UPDATE "link" SET
     tags = $1 
@@ -39,13 +31,10 @@ router.put("/:id", rejectUnauthenticated, (req, res) => {
   pool
     .query(queryString, [link.tags, link.details.id, req.user.id])
     .then((result) => {
-      // success
-      console.log("PUT successful");
       res.send(result.rows);
     })
-    .catch((err) => {
-      // failure
-      console.log("----->Error in PUT:", err);
+    .catch((error) => {
+      console.log(error);
       res.sendStatus(500);
     });
 });
