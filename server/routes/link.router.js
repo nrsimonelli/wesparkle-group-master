6f9a241858357de7c0ecb1839d93860945208b4b
“ericelvendahl” <ericelvendahl@gmail.com>
2020-09-16 14:06:06 -0500

    further cleaning

--- server/routes/link.router.js
+++ server/routes/link.router.js
@@ -6,11 +6,6 @@ const {
 } = require("../modules/authentication-middleware");
 const iplocate = require("node-iplocate");
 router.get("/", rejectUnauthenticated, (req, res) => {
-
-  //   Possible errors here if no user
-  console.log("req.user", req.user);
-  // Maybe one route for free, another for registered/logged in?
-  // if (req.user != undefined) {
   let queryString = `
   SELECT * from link
   WHERE "user_id" = $1 AND "disabled_link" = FALSE 
@@ -27,28 +22,6 @@ router.get("/", rejectUnauthenticated, (req, res) => {
     });
 });
 
-// // This route performs the redirection
-// router.get("/:short_url", (req, res) => {
-//   let queryString = `
-//     SELECT * FROM "link" WHERE short_url = '${req.params.short_url}';`;
-//   // This is how we get referer information:
-//   //let queryString2 = `INSERT INTO click (link_id, location, referral) VALUES (1, 'Namibia', '${req.headers.referer}';`;
-//   pool
-//     .query(queryString)
-//     .then((result) => {
-//       console.log(
-//         "in GET/:short_url - Trying to redirect to",
-//         result.rows[0].long_url
-//       );
-//       console.log("req.headers.referrer is", req.headers.referer);
-//       res.redirect(result.rows[0].long_url);
-//     })
-//     .catch((error) => {
-//       console.log("Error in GET/:short_url redirect. Error is", error);
-//       res.sendStatus(500);
-//     });
-// });
-// Attempt at GET /:short_url transaction
 router.get("/:short_url", async (req, res) => {
   const connection = await pool.connect();
   try {
@@ -56,14 +29,11 @@ router.get("/:short_url", async (req, res) => {
     // Get appropriate 'link' for submitted short_url
     const queryString = `
       SELECT * FROM "link" WHERE short_url = '${req.params.short_url}';`;
-    // We care about the result coming out of the database, sos sae it (need id)
     const linkRecord = await connection.query(queryString);
     // Get the id from the query result
     const longUrl = linkRecord.rows[0].long_url;
     console.log("longUrl is", longUrl);
-    // const depositStatement = `INSERT INTO register (acct_id, amount) VALUES ($1, $2);`
-    // // We don't care about the result coming back so ignore it
-    // await connection.query(depositStatement, [newAcctId, amount]);
+
     // Get client's IP
     const clientIP = req.connection.remoteAddress;
     console.log("clientIP is", clientIP);
@@ -74,7 +44,6 @@ router.get("/:short_url", async (req, res) => {
     await iplocate("66.39.154.26").then((results) => {
       console.log("results is", results);
       clientPostalCode = results.postal_code;
-
     });
     const queryString2 = `INSERT INTO click (link_id, client_ip, referrer) VALUES ($1, $2, $3);`;
     await connection.query(queryString2, [
@@ -89,9 +58,6 @@ router.get("/:short_url", async (req, res) => {
     await connection.query("ROLLBACK;");
     res.sendStatus(500);
   } finally {
-    // THIS IS ALSO REALLY IMPORTANT!!!
-    // Puts the connection back in the pool to be used again later.
-    // FREE THE CONNECTION IN FINALLY
     connection.release();
   }
 });
@@ -105,11 +71,7 @@ router.put("/:id", rejectUnauthenticated, (req, res) => {
     .catch(() => res.sendStatus(500));
 });
 router.post("/", async (req, res) => {
-  // didn't want to rejectUnauth here if non logged in users can add link
-  console.log("req.body is:", req.body);
-  console.log("req.user is:", req.user);
   const link = req.body;
-  // This seems like a janky workaround, but works for now
   if (req.user != undefined) {
     const queryString = `INSERT INTO "link" ("user_id", "long_url", "short_url", "tags")
     VALUES ($1, $2, $3, '{"example tag"}');`;
