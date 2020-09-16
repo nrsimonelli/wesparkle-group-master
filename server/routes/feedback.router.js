8a6ab4f4c36928819ad5c770984b8b775816a77b
Audry Wolters <audrywolters@gmail.com>
2020-09-16 15:07:28 -0500

    feedback form comments. spaces. changed all email data to .env so CIO can do whatever they want with it

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,23 +2,23 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 
-// BUGS in HEROKU? https://www.twilio.com/blog/working-with-environment-variables-in-node-js-html
-
 router.post("/", (req, res) => {
-  // this is the ship the user's email sails
+  // this is the ship the user's email rides
   var transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
+    host: process.env.FEEDBACK_EMAIL_HOST,
+    port: process.env.FEEDBACK_EMAIL_PORT,
     auth: {
-      user: process.env.FEEDBACK_EMAIL_USERNAME,
+      user: process.env.FEEDBACK_EMAIL_USERNAME, 
       pass: process.env.FEEDBACK_EMAIL_PASSWORD
     },
   });
 
-  // here's where we set who does what
+  // this is what has been typed by the user
+  // comes from client > FeedbackForm.js
   let mailOptions = {
     subject: 'We Sparkle User Feedback',
-    to: process.env.FEEDBACK_EMAIL,
+    // to: is the email address we want to recieve the emails 
+    to: process.env.FEEDBACK_EMAIL_ADDRESS,
     html: `<p>From: ${req.body.userName}</p>
            <p>Email: ${req.body.userEmail}</p>
            <p>Message: ${req.body.emailBody}</p>`,
